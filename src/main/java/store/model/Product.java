94a65993e4af8d47c0df89481b481faf7498b2a6
yujin-zero <poly30@naver.com>
2024-11-06 15:41:09 +0900

    feat(Product) : 재고 감소 메서드 구현
    주문 수량에 따라 재고를 감소시키는 reduceQuantity 메서드 추가

---
+++ src/main/java/store/model/Product.java
@@ -0,0 +1,19 @@
+package store.model;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+    private String promotion;
+
+    public Product(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public void reduceQuantity(int orderQuantity) {
+        quantity -= orderQuantity;
+    }
+}
