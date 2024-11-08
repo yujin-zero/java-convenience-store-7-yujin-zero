ddacb6f93d368d1627636c5d2a22afa940a27ca3
yujin-zero <poly30@naver.com>
2024-11-08 11:22:07 +0900

    feat(Promotion) :  해당 상품이 프로모션 적용 가능 상태인지 확인하는 메서드 구현

---
+++ src/main/java/store/model/Promotion.java
@@ -0,0 +1,27 @@
+package store.model;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
+
+public class Promotion {
+    private String name;
+    private int buy;
+    private int get;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public Promotion(String name, int buy, int get, LocalDateTime startDate, LocalDateTime endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean isApplicable(Product product, int quantity) {
+        LocalDateTime currentDate = DateTimes.now();
+
+        return (currentDate.isAfter(startDate) || currentDate.isEqual(startDate)) && (currentDate.isBefore(endDate)
+                || currentDate.isEqual(endDate)) && quantity >= buy;
+    }
+}
