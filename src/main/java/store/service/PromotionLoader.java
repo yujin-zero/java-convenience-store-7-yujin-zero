ee1c460233e44ea97098a2aa50c80ed64c15dbe9
yujin-zero <poly30@naver.com>
2024-11-08 18:49:24 +0900

    feat(PromotionLoader) : 파일에서 프로모션 정보를 로드하는 loadPromotions 메서드 구현

--- src/main/java/store/service/PromotionLoader.java
+++ src/main/java/store/service/PromotionLoader.java
@@ -1,11 +1,36 @@
 package store.service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 import store.model.Promotion;
 
 public class PromotionLoader {
+
+    public List<Promotion> loadPromotions(String filePath) {
+        List<Promotion> promotions = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            parsePromotionsFromFile(br, promotions);
+        } catch (IOException e) {
+            throw new RuntimeException("파일을 읽는 도중 오류가 발생했습니다: " + filePath, e);
+        }
+
+        return promotions;
+    }
+
+    private void parsePromotionsFromFile(BufferedReader br, List<Promotion> promotions) throws IOException {
+        String line = br.readLine();
+        while ((line = br.readLine()) != null) {
+            Promotion promotion = parsePromotionLine(line);
+            promotions.add(promotion);
+        }
+    }
 
     private Promotion parsePromotionLine(String line) {
         StringTokenizer tokenizer = new StringTokenizer(line, ",");
