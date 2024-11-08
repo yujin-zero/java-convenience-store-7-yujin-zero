0740c6111f20968b3a976373d97b7a91e706cdc7
yujin-zero <poly30@naver.com>
2024-11-08 23:13:35 +0900

    feat(MemebershipDiscount) : 멤버십 할인 금액 계산 로직 추가

---
+++ src/main/java/store/model/MembershipDiscount.java
@@ -0,0 +1,11 @@
+package store.model;
+
+public class MembershipDiscount {
+    private static final double DISCOUNT_RATE = 0.3;
+    private static final int MAX_DISCOUNT_AMOUNT = 8000;
+
+    public int calculateDiscountAmount(int totalAmount) {
+        int discount = (int) (totalAmount * DISCOUNT_RATE);
+        return Math.min(discount, MAX_DISCOUNT_AMOUNT);
+    }
+}
