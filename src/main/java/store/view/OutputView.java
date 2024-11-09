6254fe48f105b2e59c77725074e398fc7967abae
yujin-zero <poly30@naver.com>
2024-11-09 19:45:25 +0900

    feat(OutView) : 인삿말 출력 메서드 printWelcomeMessage 추가

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,10 @@
+package store.view;
+
+public class OutputView {
+
+    public static void printWelcomeMessage() {
+        System.out.println("안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
+
+}
