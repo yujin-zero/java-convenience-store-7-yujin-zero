2c7a4ef605bcb3297a517783c287f17d6e4af928
yujin-zero <poly30@naver.com>
2024-11-09 20:53:40 +0900

    feat(Application): 결제 시스템 초기화를 위한 Application 클래스 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,30 @@
 package store;
 
+import java.util.List;
+import store.controller.PaymentSystem;
+import store.model.Cart;
+import store.model.Product;
+import store.model.Promotion;
+import store.service.ProductLoader;
+import store.service.PromotionLoader;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PromotionLoader promotionLoader = new PromotionLoader();
+        List<Promotion> promotions = promotionLoader.loadPromotions("src/main/resources/promotions.md");
+
+        ProductLoader productLoader = new ProductLoader(promotions);
+        List<Product> products = productLoader.loadProduct("src/main/resources/products.md");
+
+        Cart cart = new Cart();
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        PaymentSystem paymentSystem = new PaymentSystem(products, cart, inputView, outputView);
+
+        paymentSystem.start();
     }
 }
