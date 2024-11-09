cc8929b89217aaa497ca238d3981462157ee67c6
yujin-zero <poly30@naver.com>
2024-11-09 17:08:21 +0900

    test(Cart): 장바구니 총 가격 계산 테스트 추가

--- src/test/java/store/model/CartTest.java
+++ src/test/java/store/model/CartTest.java
@@ -2,41 +2,37 @@ package store.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CartTest {
 
-//    외부에서 검증 예정
-//    @Test
-//    @DisplayName("장바구니에 아이템이 정상적으로 추가되는지 테스트")
-//    public void testAddCartItem() {
-//        Product product = new Product("사과", 1000, 10, null);
-//        CartItem cartItem = new CartItem(product, 2);
-//
-//        Cart cart = new Cart();
-//        cart.addCartItem(cartItem);
-//
-//        List<CartItem> cartItems = cart.getCartItems();
-//        assertEquals(1, cartItems.size(), "장바구니에 아이템이 하나 추가되어야 합니다.");
-//        assertEquals(cartItem, cartItems.get(0), "추가된 아이템이 CartItem과 일치해야 합니다.");
-//
-//    }
+    private Cart cart;
+
+    @BeforeEach
+    public void setup() {
+        cart = new Cart();
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, LocalDateTime.now().minusDays(1),
+                LocalDateTime.now().plusDays(1));
+        Product product1 = new Product("콜라", 1000, 10, promotion);
+        Product product2 = new Product("사탕", 200, 10, null);
+        cart.addCartItem(new CartItem(product1, 3));
+        cart.addCartItem(new CartItem(product2, 2));
+    }
 
     @Test
     @DisplayName("장바구니 전체 수량 계산 테스트")
     public void testCalculateItemCountInCart() {
-        Product product1 = new Product("사탕", 500, 10, null);
-        Product product2 = new Product("초콜릿", 1000, 10, null);
-        CartItem cartItem1 = new CartItem(product1, 2);
-        CartItem cartItem2 = new CartItem(product2, 3);
-
-        Cart cart = new Cart();
-        cart.addCartItem(cartItem1);
-        cart.addCartItem(cartItem2);
-
         int totalCount = cart.calculateItemCountInCart();
         assertEquals(5, totalCount, "장바구니의 전체 수량이 예상과 다릅니다");
     }
 
+    @Test
+    @DisplayName("총 금액 계산 테스트")
+    public void testCalculateTotalPrice() {
+        int totalPrice = cart.calculateTotalPrice();
+        assertEquals(3400, totalPrice);
+    }
 }
