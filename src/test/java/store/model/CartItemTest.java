5ec61306c7451f53f3efa47c5ce3624e91f1e448
yujin-zero <poly30@naver.com>
2024-11-09 16:05:24 +0900

    test(CartItem) : calculatePromotionDiscountPrice 메서드에 대한 단위 테스트 추가

--- src/test/java/store/model/CartItemTest.java
+++ src/test/java/store/model/CartItemTest.java
@@ -2,6 +2,7 @@ package store.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -17,4 +18,28 @@ class CartItemTest {
 
         assertEquals(3000, totalPrice, "총 가격이 예상과 일치하지 않습니다.");
     }
+
+    @Test
+    @DisplayName("프로모션 할인 금액 계산 테스트")
+    public void testCalculatePromotionDiscountPrice() {
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, LocalDateTime.now().minusDays(1),
+                LocalDateTime.now().plusDays(1));
+        Product product = new Product("콜라", 1000, 10, promotion);
+        CartItem item = new CartItem(product, 5);
+
+        int discountPrice = item.calculatePromotionDiscountPrice();
+        assertEquals(1000, discountPrice);
+    }
+
+    @Test
+    @DisplayName("프로모션 없을 때 할인 금액 0인지 테스트")
+    public void testCalculatePromotionDiscountPrice_NoPromotion() {
+        Product product = new Product("콜라", 1000, 10, null);
+        CartItem item = new CartItem(product, 5);
+
+        int discountPrice = item.calculatePromotionDiscountPrice();
+        assertEquals(0, discountPrice);
+    }
+
+    
 }
