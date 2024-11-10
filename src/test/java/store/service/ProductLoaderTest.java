5db36cce1fedf3918c96570ecac4423f165d97f6
yujin-zero <poly30@naver.com>
2024-11-10 16:48:08 +0900

    test(ProductLoaderTest) : 프로모션 제품 아래 일반 제품 추가 로직에 따른 테스트 수정

--- src/test/java/store/service/ProductLoaderTest.java
+++ src/test/java/store/service/ProductLoaderTest.java
@@ -29,32 +29,38 @@ class ProductLoaderTest {
         }
 
         List<Promotion> promotions = new ArrayList<>();
-        promotions.add(new Promotion("탄산2+1", 3, 1, LocalDate.of(2024, 11, 1).atStartOfDay(),
+        promotions.add(new Promotion("탄산2+1", 2, 1, LocalDate.of(2024, 11, 1).atStartOfDay(),
                 LocalDate.of(2024, 12, 1).atStartOfDay()));
 
         ProductLoader loader = new ProductLoader(promotions);
         List<Product> products = loader.loadProduct(tempFile.toString());
 
         assertNotNull(products, "Product 리스트가 null이 아니어야 합니다.");
-        assertEquals(3, products.size(), "리스트의 상품 개수가 예상과 다릅니다.");
+        assertEquals(4, products.size(), "리스트의 상품 개수가 예상과 다릅니다.");
 
         Product product1 = products.get(0);
         assertEquals("콜라", product1.getName());
         assertEquals(1000, product1.getPrice());
-//        assertEquals(10, product1.getQuantity());
+        assertEquals(10, product1.getQuantity());
         assertEquals("탄산2+1", product1.getPromotion().getName());
 
         Product product2 = products.get(1);
-        assertEquals("사이다", product2.getName());
-        assertEquals(1200, product2.getPrice());
-//        assertEquals(5, product2.getQuantity());
+        assertEquals("콜라", product2.getName());
+        assertEquals(1000, product2.getPrice());
+        assertEquals(0, product2.getQuantity());
         assertEquals(null, product2.getPromotion());
 
         Product product3 = products.get(2);
-        assertEquals("물", product3.getName());
-        assertEquals(500, product3.getPrice());
-//        assertEquals(20, product3.getQuantity());
+        assertEquals("사이다", product3.getName());
+        assertEquals(1200, product3.getPrice());
+        assertEquals(5, product3.getQuantity());
         assertEquals(null, product3.getPromotion());
+
+        Product product4 = products.get(3);
+        assertEquals("물", product4.getName());
+        assertEquals(500, product4.getPrice());
+        assertEquals(20, product4.getQuantity());
+        assertEquals(null, product4.getPromotion());
 
         Files.delete(tempFile);
     }
