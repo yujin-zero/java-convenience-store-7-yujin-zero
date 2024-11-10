2e7dbe07dba42154b152837521af1779cf4e60eb
yujin-zero <poly30@naver.com>
2024-11-10 20:14:59 +0900

    feat(InputView) : 프로모션 추가 증정 여부 확인 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -31,6 +31,12 @@ public class InputView {
         return input.equalsIgnoreCase("Y");
     }
 
+    public static boolean confirmAddtionPromotionItem(String productName, int productCount) {
+        System.out.printf("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", productName, productCount);
+        String input = Console.readLine();
+        return input.equalsIgnoreCase("Y");
+    }
+
     private static Map<String, Integer> parsePurchaseInput(String input) {
         Map<String, Integer> items = new LinkedHashMap<>();
         StringTokenizer pairTokenizer = new StringTokenizer(input.replaceAll("\\[|\\]", ""), ",");
